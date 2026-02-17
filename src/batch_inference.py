ccfcbf94ecdc11ab991338f593cb66c6864acbcd
Moiz Ahmed Arain <153999362+MoizAhmedArain@users.noreply.github.com>
2026-02-18 00:04:17 +0500

    Fix feature group retrieval in batch inference

--- src/batch_inference.py
+++ src/batch_inference.py
@@ -47,6 +47,8 @@ def main():
         # 3. Clean column names (handles the dictionary format bug automatically)
         # 1. Access the Feature Group directly (bypassing the broken Feature View)
         aqi_fg = fs.get_feature_group(name="karachi_aqi_weather", version=1)
+        aqi_fg = fs.get_feature_groups()
+        print([fg.name for fg in aqi_fgs])
 
         # 2. Force the use of the Python engine to bypass the Hive/Query Service error
         hist_df = aqi_fg.read(read_options={"use_hive": False})
